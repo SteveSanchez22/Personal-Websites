1993dee23e0e9d72d1dbc85acb0515cd70f70801
Steve Alejandro Sanchez <sasanch22@gmail.com>
2022-02-28 18:13:56 +0000

    Imported from test enviroment

--- II/index.php
+++ II/index.php
@@ -1,31 +1,34 @@
+<!-- Version 2.3 -->
+
 <?php
 
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
 
 $message_sent = false;
 
-    if(isset($_POST['email']) && $_POST['email'] != '') {
+if (isset($_POST['email']) && $_POST['email'] != '') {
 
-        if(filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
+    if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
 
-            // Local Variables
-            $userName = $_POST['name'];
-            $userEmail = $_POST['email'];
-            $messageSubject = $_POST['subject'];
-            $message = $_POST['message'];
+        // Submit form here
+        $userName = $_POST['name'];
+        $userEmail = $_POST['email'];
+        $messageSubject = $_POST['subject'];
+        $message = $_POST['message'];
 
-            $to = "me@stevealejandrosanchez.com";
-            $body = "";
+        $to = "me@stevealejandrosanchez.com";
+        $body = "";
 
-            $body .= "From : ".$userName. "\r\n";
-            $body .= "Email : ".$userEmail. "\r\n";
-            $body .= "Message : ".$message. "\r\n";
+        $body .= "From : " . $userName . "\r\n";
+        $body .= "Email : " . $userEmail . "\r\n";
+        $body .= "Message : " . $message . "\r\n";
 
-
-            mail($to, $messageSubject, $body);
-            $message_sent = true;
-
-        }
+        mail($to, $messageSubject, $body);
+        $message_sent = true;
     }
+}
 
 ?>
 
@@ -35,33 +38,67 @@ $message_sent = false;
 
 <head>
     <!-- Google Tag Manager -->
-    <script>(function (w, d, s, l, i) {
-            w[l] = w[l] || []; w[l].push({
-                'gtm.start':
-                    new Date().getTime(), event: 'gtm.js'
-            }); var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
-                    'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-        })(window, document, 'script', 'dataLayer', 'GTM-WZJR3D9');</script>
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-WZJR3D9');
+    </script>
     <!-- End Google Tag Manager -->
+
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-209079910-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-209079910-1');
+    </script>
+
+
+    <!-- START OF META -->
 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Meta -->
-    <meta name="description"
-        content="Steve Alejandro Sanchez (Psst the key to pronouncing my name is start English and transition to a Spanish accent), a London based junior developer. Explore more about me, what I am getting up to and more!">
+    <meta name="description" content="Steve Alejandro Sanchez (Psst the key to pronouncing my name is start English and transition to a Spanish accent), a London based junior developer. Explore more about me, what I am getting up to and more!">
     <meta name="keywords" content="steve sanchez, steve alejandro sanchez, junior developer">
+    <meta property="og:site_name" content="Steve Alejandro Sanchez" />
+    <meta property="og:locale" content="en_GB" />
     <meta http-equiv="content-language" content="en-gb">
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Steve Alejandro Sanchez Portfolio">
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image" content="https://stevealejandrosanchez.com/images/Socials.jpg" />
+    <meta property="og:image:width" content="1200" />
+    <meta property="og:image:height" content="630" />
+    <meta property="og:image:alt" content="The Man Himself ..... Steve Alejandro Sanchez" />
+    <meta property="og:description" content="Steve Alejandro Sanchez (Psst the key to pronouncing my name is start English and transition to a Spanish accent), a London based junior developer. Explore more about me, what I am getting up to and more!">
+    <meta property="og:url" content="https://stevealejandrosanchez.com" />
     <meta name="author" content="Steve Alejandro Sanchez">
+
+    <!--  ===== END OF META  ======= -->
 
     <!--  Custom CSS -->
     <link rel="stylesheet" href="style.css">
 
     <!-- Ubuntu GGL Font -->
-    <link
-        href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
 
     <!-- magnific-popup.js - magnific-popup.min.css -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css">
@@ -70,22 +107,17 @@ $message_sent = false;
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     <!-- JQUERY CDNJS  -->
-    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <!-- magnific-popup.js - magnific-popup.min.js -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
-
-    <!-- Custom JS -->
-    <script src="script.js"></script>
 
     <title>Steve Alejandro Sanchez Portfolio</title>
 </head>
 
 <body>
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZJR3D9" height="0" width="0"
-            style="display:none;visibility:hidden"></iframe></noscript>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZJR3D9" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
     <!-- Navbar -->
@@ -117,8 +149,7 @@ $message_sent = false;
                 <div class="sm">
                     <a href="https://github.com/SteveSanchez22" target="__Blank" class="fab fa-github"></a>
                     <!-- <a href="#" class="fab fa-instagram"></a> -->
-                    <a href="https://www.linkedin.com/in/steve-alejandro-sanchez/" target="__Blank"
-                        class="fab fa-linkedin-in"></a>
+                    <a href="https://www.linkedin.com/in/steve-alejandro-sanchez/" target="__Blank" class="fab fa-linkedin-in"></a>
                 </div>
                 <div class="buttons">
                     <a href="#contact">Contact me</a>
@@ -133,14 +164,13 @@ $message_sent = false;
     </section>
 
     <!-- Home  -->
-
     <section id="about">
         <div class="inner-width">
             <h1 class="section-title">About</h1>
             <div class="about-content">
                 <img src="images/Steve-sanchez.jpg" alt="steve-alejandro-sanchez" class="about-pic">
                 <div class="about-text">
-                    <h2>Steve Alejandro Sanchez</h2>
+                    <h2>A bit about me</h2>
                     <h3>
                         <span>Developer</span>
                         <span>Designer</span>
@@ -148,23 +178,12 @@ $message_sent = false;
                     </h3>
                     <p>
 
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique facilis, impedit, rem maxime
-                        quo, atque voluptates reiciendis labore praesentium dignissimos nostrum tempore ducimus vitae!
-                        Quas totam veniam et tempora quis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic
-                        natus provident non eius quas, culpa nisi numquam fugit cumque, at qui alias fuga commodi maxime
-                        explicabo quisquam nesciunt laudantium! Atque.
-
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia, fugiat quas voluptas
-                        molestiae dolorum dolore. Hic alias nostrum impedit quas, autem, quisquam tenetur, tempore
-                        praesentium suscipit eos ipsum doloribus architecto.
-                        Commodi, culpa nisi cum ea praesentium nam corrupti esse libero distinctio dolores dolorem
-                        provident cumque quam natus obcaecati, excepturi eum exercitationem. Beatae veritatis quibusdam
-                        repudiandae voluptatem, pariatur ex necessitatibus vitae.
-                        Deserunt corporis voluptates hic distinctio aperiam, numquam velit error amet molestiae et quia
-                        aliquam vel assumenda totam sed ab. Omnis quis autem ipsam qui, beatae labore hic quaerat maxime
-                        nemo!
-
-
+                        Hi, my name is Steve Alejandro Sanchez. I'm an aspiring Junior Developer with a passion for human-centred design.
+                        My intrest in web development started back in 2016 when I began my degree in Computer Science at the <a href="https://www.kent.ac.uk" target="_blank"> University of Kent.</a>
+                        <br>
+                        <br>
+                        Fast-Forward to today, and I've had the privilege of working at a Brand Asset Management company that managed
+                        Luxury Brands. My main focus these days is in building accessible, inclusive products and digital experiences.
                     </p>
                 </div>
             </div>
@@ -226,8 +245,8 @@ $message_sent = false;
         </div>
     </section>
 
-    <!-- Education -->
 
+    <!-- Education -->
     <section id="education">
         <div class="inner-width">
             <h1 class="section-title">Education & Experiences</h1>
@@ -286,8 +305,7 @@ $message_sent = false;
                         My journey to University was very much ingrained from very young. Although education has played
                         a major role in my life, I have always struggled, Until I met my private tutor at the age of 10.
                         He turned an under achieving student into a person who exceeded teachers result predictions. He
-                        has
-                        taught me life lessons, which to this day I use!
+                        has taught me life lessons, which to this day I use!
                     </p>
                 </div>
             </div>
@@ -340,8 +358,7 @@ $message_sent = false;
                     </div>
                 </a>
 
-                <a href="https://github.com/SteveSanchez22" target="_blank" title="Directing to Steve's Github"
-                    class="work">
+                <a href="https://github.com/SteveSanchez22" target="_blank" title="Directing to Steve's Github" class="work">
                     <img src="images/Red.webp" alt="">
                     <div class="info">
                         <h3>Work Title</h3>
@@ -353,17 +370,16 @@ $message_sent = false;
     </section>
 
     <!-- Contact Section -->
-
     <section id="contact">
         <div class="inner-width">
             <h1 class="section-title">Get in touch</h1>
-
+            <!-- Contact Form -->
             <form action="index.php" method="POST" class="contact-form">
-                <input type="text" class="userName" name="name" placeholder="Name" required>
-                <input type="email" class="userEmail" name="email" placeholder="Email" required>
+                <input type="text" class="userName" name="name" placeholder="Name">
+                <input type="email" class="userEmail" name="email" placeholder="Email">
                 <input type="text" class="userSubject" name="subject" placeholder="Subject">
-                <textarea class="userMessage" name="message" placeholder="Message" required></textarea>
-                <input type="submit" value="Send Message" class="btn">
+                <textarea class="userMessage" name="message" placeholder="Message"></textarea>
+                <input type="submit" value="Send" class="btn">
             </form>
         </div>
     </section>
@@ -372,16 +388,21 @@ $message_sent = false;
     <footer>
         <div class="inner-width">
             <div class="copyright">
-                copyright &copy; 2021 | Steve Alejandro Sanchez. All rights reserved.
+                copyright &copy; 2022 | Steve Alejandro Sanchez. All rights reserved.
             </div>
             <div class="sm">
                 <a href="https://github.com/SteveSanchez22" target="__Blank" class="fab fa-github"></a>
-                <!-- <a href="#" class="fab fa-instagram"></a> -->
-                <a href="https://www.linkedin.com/in/steve-alejandro-sanchez/" target="__Blank"
-                    class="fab fa-linkedin-in"></a>
+                <a href="https://www.instagram.com/alejo_sancho/" target="__Blank" class="fab fa-instagram"></a>
+                <a href="https://www.linkedin.com/in/steve-alejandro-sanchez/" target="__Blank" class="fab fa-linkedin-in"></a>
             </div>
         </div>
     </footer>
+
+    <div id="progress-bar"></div>
+
+    <!-- Custom JS -->
+    <script src="script.js"></script>
+
 </body>
 
 </html>
